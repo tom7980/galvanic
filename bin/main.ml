572e476be66fd30c7e511945d0acd6304ab8b94f
Tom Farmer <tfarmer7980@gmail.com>
2026-07-03 23:47:18 +0100

    Add basic function calls

--- bin/main.ml
+++ bin/main.ml
@@ -88,7 +88,7 @@ let rec chainr1 x op =
 let chainr x op default = chainr1 x op <|> return default
 
 let satisfy test =
-  any >>= (fun res -> if test res then return res else mzero)
+  any >>= (fun res -> Printf.printf "Test: %c \n" res; if test res then begin Printf.printf "Matches: %c \n" res; return res end else mzero)
 
 let range low high = satisfy (fun x -> low <= x && x <= high)
 
@@ -125,6 +125,8 @@ let reserved = [
   "and";
   "or";
   "fn";
+  "let";
+  "ret";
 ]
 
 exception Syntax_error
@@ -151,13 +153,14 @@ type expr = Var of string
           | Bool of bool
           | BoolOp of op * expr * expr
           | Args of expr list
+          | Call of string * expr
+          | Unit
 
 type stmt = Assign of string * expr 
           | Stmts of stmt list
           | If of expr * stmt * stmt
           | While of expr * stmt
-          | FnDef of string * expr * stmt
-          | Call of string * expr
+          | FnDef of string * string list * stmt * expr
 
 let parens = between (token "(") (token ")")
 
@@ -168,6 +171,8 @@ let number =
 let var =
   let* s = ident in
   return (Var s)
+
+
 
 let true_p = token "true" >> return (Bool true)
 let false_p = token "false" >> return (Bool false)
@@ -182,23 +187,32 @@ let andop = token "and" >> return (fun x y -> BoolOp(And, x, y))
 let orop = token "or" >> return (fun x y -> BoolOp(Or, x, y))
 let gtop = token ">" >> return (fun x y -> BoolOp(GT, x, y))
 let ltop = token "<" >> return (fun x y -> BoolOp(LT, x, y))
+    
 
 let rec expr input = (chainl1 and_expr orop) input
 and and_expr input = (chainl1 rop_expr andop) input
 and rop_expr input = (chainl1 add_expr (ltop <|> gtop)) input
 and add_expr input = (chainl1 mul_expr (addop <|> minusop)) input
-and mul_expr input = (chainl1 prim_expr (mulop <|> divop)) input
+and mul_expr input = (chainl1 call_expr (mulop <|> divop)) input
+and call_expr input = (call <|> prim_expr) input
 and prim_expr input = (parens expr <|> atom) input
+and args input =
+  (let* l = sep_by1 expr (token ",") in
+  return (Args l)) input
+and call input =
+  (let* name = ident
+   and* args = token "(" >> args in
+   token ")" >> return (Call(name, args))) input
 
-let args =
-  let* l = sep_by1 expr (token ",") in
-  return (Args l)
+let argnames =
+  let* l = sep_by1 ident (token ",") in
+  return l
 
 let rec stmts input =
   (let* l = sep_by1 stmt (token ";") in
   return (Stmts l)) input
 and stmt input =
-  (if_stmt <|> while_stmt <|> assign_stmt) input
+  (if_stmt <|> while_stmt <|> assign_stmt <|> fn_stmt) input
 and if_stmt input =
   (let* pred = token "if" >> expr
   and* thn = token "then" >> token "{" >> stmts
@@ -214,9 +228,10 @@ and assign_stmt input =
   return (Assign(var, value))) input
 and fn_stmt input =
   (let* name = token "fn" >> ident
-   and* args = token "(" >> args
-   and* body = token ")" >> token "{" >> stmts in
-   token "}" >> return (FnDef(name, args, body))) input
+   and* args = token "(" >> argnames
+   and* body = token ")" >> token "{" >> end_by1 stmt (token ";") >>= fun l -> return (Stmts l)
+   and* rtrn = option Unit expr in
+   token "}" >> return (FnDef(name, args, body, rtrn))) input
 
 let program = stmts << (spaces << eof ())
 
@@ -240,8 +255,28 @@ module Envir = struct
   let set_var env x value =
     Hashtbl.replace env.vars x value
 
+  let push_func env x args body return =
+    Hashtbl.replace env.funcs x (body, args, return)
+
+  let get_func env x =
+    Hashtbl.find env.funcs x
+
+  let make_scoped_env env = {
+      vars = Hashtbl.copy env.vars;
+      funcs = Hashtbl.copy env.funcs;
+    }
 end
 
+let pp_expr expr =
+  match expr with
+  | Args _ -> "Args"
+  | Var _ -> "Var"
+  | Num _ -> "Num"
+  | Arith _ -> "Arith"
+  | Bool _ -> "Bool"
+  | BoolOp _ -> "BoolOp"
+  | Call _ -> "Call"
+  | Unit -> "Unit"
 
 let rec eval env prog =
   match prog with
@@ -267,9 +302,27 @@ let rec eval env prog =
       else ()
     in
     loop ()
-  | _ -> raise Runtime_error
+  | FnDef (name, args, body, return) ->
+    Envir.push_func env name args body return
     
-    
+and eval_func env args def_args body return =
+  let scoped_env = Envir.make_scoped_env env in
+  let vals = eval_args scoped_env args in
+  List.iter2 (fun name value -> Envir.set_var scoped_env name value) def_args vals;
+  eval scoped_env body;
+  eval_arith scoped_env return
+
+and eval_args env expr =
+  match expr with
+  | Args (x :: xs) ->
+    let r = eval_arith env x in
+    r :: eval_args env (Args xs)
+  | Args [] ->
+    []
+  | _ ->
+    Printf.printf "Args eval: %s" (pp_expr expr);
+    raise Runtime_error
+
 
 and eval_arith env expr =
   match expr with
@@ -291,7 +344,16 @@ and eval_arith env expr =
     lhs' / rhs'
   | Var x -> Envir.get_var env x
   | Num n -> n
-  | _ -> raise Runtime_error
+  | Unit -> 0
+  | Call (name, args) -> begin
+    match Envir.get_func env name with 
+    | (body, def_args, return) -> eval_func env args def_args body return
+  end 
+  | _ -> begin
+      Printf.printf "Runtime error in eval arith\n";
+      Printf.printf "Eval expr: %s\n" (pp_expr expr);
+      raise Runtime_error
+    end
 
 and eval_bool env expr =
   match expr with
@@ -312,19 +374,27 @@ and eval_bool env expr =
     and y' = eval_arith env y in
     x' < y'
   | Bool x -> x
-  | _ -> raise Runtime_error
+  | _ -> begin
+      Printf.printf "Runtime error in eval bool\n";
+      raise Runtime_error
+    end
 
 let in_str = "fact = 1 ;
 val = 10000 ;
 cur = val ;
 mod = 1000000007 ;
 
-while ( cur > 1 )
+count = 0 ;
+
+fn add(x) { x = x + 1 ; x } ;
+
+while ( cur > 0 )
   do
    {
       fact = fact * cur ;
       fact = fact - fact / mod * mod ;
-      cur = cur - 1
+      cur = cur - 1 ;
+      count = add(count)
    } ;
 
 cur = 0"
